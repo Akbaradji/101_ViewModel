02da5b37b5f4e6962730885421ce5bfc9b0a4bae
Akbaradji <akbaradjiherlambang@gmail.com>
2023-10-21 15:30:02 +0700

    Finish

---
+++ app/src/main/java/com/example/praktikum5/data/dataform.kt
@@ -0,0 +1,5 @@
+package com.example.praktikum5.data
+
+data class dataform(
+    val sex: String = ""
+)
---
+++ app/src/main/java/com/example/praktikum5/data/datasource.kt
@@ -0,0 +1,12 @@
+package com.example.praktikum5.data
+
+import com.example.praktikum5.R
+
+object datasource {
+    val jenis = listOf(
+        R.string.laki,
+        R.string.wanita,
+        R.string.wanita2
+
+    )
+}
